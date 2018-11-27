346f359eaaec6565d26ba8f77ce9a693efb659da
Will Faithfull <willfaithfull89@gmail.com>
2018-11-27 11:05:07 +0000

    Initial commit of base spring project

---
+++ gtagged/src/main/java/org/codeforlancaster/gtagged/GtaggedApplication.java
@@ -0,0 +1,12 @@
+package org.codeforlancaster.gtagged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtaggedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtaggedApplication.class, args);
+	}
+}
