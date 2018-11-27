58a48506620166fcb62df83d88f057d7f8fb4aef
Will Faithfull <willfaithfull89@gmail.com>
2018-11-27 11:56:12 +0000

    Create image repository

---
+++ gtagged/src/main/java/org/codeforlancaster/gtagged/images/ImageRepository.java
@@ -0,0 +1,14 @@
+package org.codeforlancaster.gtagged.images;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * Created by wfaithfull on 27/11/18.
+ */
+@RepositoryRestResource(exported = false)
+public interface ImageRepository extends PagingAndSortingRepository<Image, String> {
+
+
+
+}
